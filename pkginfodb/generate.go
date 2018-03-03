bc1407b08e25038328faa398f6f07f497bcae12e
Alexey Gorshkov <pexuone@gmail.com>
2018-03-03 06:22:52 +0300

    removed unneeded metadata from database code

--- pkginfodb/generate.go
+++ pkginfodb/generate.go
@@ -1,3 +1,3 @@
 package pkginfodb
 
-//go:generate go-bindata -prefix infojson -pkg db -o db/db.go ../cmd/aipinfoeditor/infojson
+//go:generate go-bindata -nometadata -prefix infojson -pkg db -o db/db.go ../cmd/aipinfoeditor/infojson
